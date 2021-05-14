6325c5c58fc389b3df85726e7004154e29161864
Bigbimbs <bamidayo10@gmail.com>
2021-05-14 23:28:42 +0200

    Assignment done in via LoginActivity.kt and activity_login.xml files

--- app/src/main/java/com/example/myfirstapp/LoginActivity.kt
+++ app/src/main/java/com/example/myfirstapp/LoginActivity.kt
@@ -9,39 +9,30 @@ import android.widget.Toast
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        //setupSimpleSpinner()
-
-        //signIn()
     }
 
     private fun signIn() {
-        val email = findViewById<TextInputEditText>(R.id.email_edit_text)
-        val pass = findViewById<TextInputEditText>(R.id.password_edit_text)
-        val signInButton = findViewById<MaterialButton>(R.id.next_button)
+        val email = findViewById<TextInputEditText>(R.id.user_email)
+        val pass = findViewById<TextInputEditText>(R.id.user_password)
+        val loginInButton = findViewById<MaterialButton>(R.id.login_button)
 
-        signInButton.setOnClickListener() {
+        loginInButton.setOnClickListener() {
 
-            if (email.toString().isNullOrEmpty() || pass.toString().isNullOrEmpty()){
+            if (email.toString().isNullOrEmpty() || pass.toString().isNullOrEmpty()) {
                 Toast.makeText(this, "Email or Password is not provided", Toast.LENGTH_LONG).show()
             } else {
-                if(email.toString() == "man@gmail.com" && pass.toString() == "12345") {
+                if (email.toString() == "name@admin.com" && pass.toString() == "admin") {
                     val intent = Intent(this, ViewActivity::class.java)
                     startActivity(intent)
                 } else {
                     Toast.makeText(this, "Wrong email or password", Toast.LENGTH_LONG).show()
                 }
             }
-
+        }
         }
     }
-
-    //private fun setupSimpleSpinner() {
-     //   val spinner = findViewById<Spinner>(R.id.simple_spinner)
-     //   val spinner2: Spinner = findViewById(R.id.simple_spinner)
-    //}
-}
